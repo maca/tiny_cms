dc14459acffc89ee1c343eb890191466a85dafab
Macario Ortega <macarui@gmail.com>
2010-10-27 17:08:45 -0500

    missing field in form

--- test/rails_app/config/routes.rb
+++ test/rails_app/config/routes.rb
@@ -1,5 +1,4 @@
 ActionController::Routing::Routes.draw do |map|
-  map.dummy '/perma', :controller => 'dummy', :action => 'index'
   map.resources :pages, :except => [:new], :collection => {:reorder => :put}
   map.all '*path', :controller  => :pages, :action => :show
 end
